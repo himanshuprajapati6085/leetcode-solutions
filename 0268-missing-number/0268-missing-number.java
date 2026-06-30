19a06120f950d39f23c78716bb6d345795459c1d
himanshuprajapati6085 <himanshuprajapati1682006@gmail.com>
2026-06-30 23:58:37 +0530

    Time: 0 ms (100%), Space: 46.9 MB (97.42%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,22 +1,11 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int min_num = nums[0];
-        int max_num = nums[0];
-        for (int i = 0; i < nums.length; i++) {
-            min_num = Math.min(min_num, nums[i]);
-            max_num = Math.max(max_num, nums[i]);
+        int n = nums.length;
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+        for (int num : nums) {
+            actualSum += num;
         }
-        for (int i = 0; i <= nums.length; i++) {
-            boolean found = false;
-            for (int j = 0; j < nums.length; j++) {
-                if (nums[j] == i) {
-                    found = true;
-                    break;
-                }
-            }
-            if (!found)
-                return i;
-        }
-        return -1;
+        return expectedSum - actualSum;
     }
 }
